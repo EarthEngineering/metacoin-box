4c2005cebeb709430c1fcfdd6b709807ab7b6c1c
Gabriel Cardona <cgcardona@gmail.com>
2019-12-04 23:16:53 -0800

    Rename to EARTH and Ohio.

--- earthbox.js
+++ earthbox.js
@@ -12,32 +12,33 @@ Create a .env file (it must be gitignored) containing something like
 
 Then, run the migration with:
 
-  source .env && tronbox migrate --network mainnet
+  source .env && earthbox migrate --network mainnet
 
 */
       userFeePercentage: 100,
       feeLimit: 1e8,
-      fullHost: 'https://api.trongrid.io',
-      network_id: '1'
+      fullHost: "https://rest.earth.engineering",
+      network_id: "1"
     },
-    shasta: {
-      privateKey: process.env.PRIVATE_KEY_SHASTA,
+    ohio: {
+      privateKey: process.env.PRIVATE_KEY_OHIO,
       userFeePercentage: 50,
       feeLimit: 1e8,
-      fullHost: 'https://api.shasta.trongrid.io',
-      network_id: '2'
+      fullHost: "https://rest.ohio.earth.engineering",
+      network_id: "2"
     },
     development: {
-      // For trontools/quickstart docker image
-      privateKey: 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
+      // For earthengineering/quickstart docker image
+      privateKey:
+        "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
       userFeePercentage: 0,
       feeLimit: 1e8,
-      fullHost: 'http://127.0.0.1:' + port,
-      network_id: '9'
+      fullHost: "http://127.0.0.1:" + port,
+      network_id: "9"
     },
     compilers: {
       solc: {
-        version: '0.5.4'
+        version: "0.5.4"
       }
     }
   }
